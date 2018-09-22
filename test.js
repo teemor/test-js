4d9395e74c61fe34c99ae162256e83e1b017adef
dizhaolu <1130477946@qq.com>
2018-09-22 11:23:57 +0800

    es6语法练习

--- test.js
+++ test.js
@@ -1,15 +1,6 @@
-var s = (function () {
-    // 参数：传递给单例的一个参数集合
-    function public(args) {
-        //设置args变量为接收的参数或者为空（如果没有提供的话）
-        var args = args || {};
-        //设置name参数
-        this.name='s'
-        //设置pointX的值
-        this.pointX=args.pointX || 6;//从接收的参数里获取，或者设置为默认值
-        //设置pointY的值
-        this.pointY = args.pointY || 10;
-    }
-    //实例容器
-    var instance
-})
+var httpServer = require('http')
+httpServer.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.write("hehe")
+    res.end("hhh")
+}).listen(2333)
